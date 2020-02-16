7b5cffea42a0573ff3839725c5cedb5b7261c030
essamkomo <48596199+essamkomo@users.noreply.github.com>
2020-02-16 13:08:37 +0300

    updated of stepper motor move

--- sketch_feb12a.ino
+++ sketch_feb12a.ino
@@ -2,23 +2,31 @@
 
 // add the number of the used pins 
  int const pin1= 9;
- int const pin2= 10;
- int const pin3= 11;
+ int const pin2= 11;
+ int const pin3= 10;
  int const pin4= 12;
- 
+ int count = 0; 
 Stepper myStepper(200,pin1,pin2,pin3,pin4); 
 
 void setup() {
+Serial.begin(9600);
+// set the speed of the motor
+myStepper.setSpeed(100);
+//myStepper.step(1024);
+delay(5000); 
 
- // set the speed of the motor
-myStepper.setSpeed(90);
 }
 
 void loop() {
 
 // sett the number of steps 
-myStepper.step(1024);
 
 // delay for the rotation 
-delay(6000);
+
+//count++;
+Serial.println(count);
+delay(5000); 
+
+myStepper.step(360);
+
 }
