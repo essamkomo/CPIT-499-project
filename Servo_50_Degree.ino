6a721b47dfa7f3fb2b6c02f6bc36bac6a7b45216
essamkomo <48596199+essamkomo@users.noreply.github.com>
2020-02-24 16:55:04 +0300

    Add files via upload

---
+++ Servo_50_Degree.ino
@@ -0,0 +1,38 @@
+# include <Servo.h> 
+
+Servo Servo1 ;
+
+int Position=0 ;
+
+int mode=0;
+void setup() {
+Serial.begin(9600);
+
+Servo1.attach(A0); 
+}
+
+void loop() {
+if(mode==0){
+Position++;
+Servo1.write(Position);
+Serial.println(Position);
+delay(15);
+if(Position==50){
+mode=1;    
+
+}
+
+}else{
+  
+Position--;
+Servo1.write(Position);
+Serial.println(Position);
+delay(15);
+if(Position==0){
+mode=0;    
+
+} 
+ 
+}
+
+}
