0ad72aa8479a125e6c5a53f3f5112c331ff80733
essamkomo <48596199+essamkomo@users.noreply.github.com>
2020-02-29 17:07:40 +0300

    22 feb

---
+++ myesp.h
@@ -0,0 +1,43 @@
+
+#include <ESP8266WiFi.h> 
+
+class WIFI{
+  String NET_SSID = "";
+  String NET_PASSWORD = "";
+  
+ 
+  public :
+ void connection(){
+    
+  // connect to wifi.
+  WiFi.begin(NET_SSID, NET_PASSWORD);
+  Serial.print("connecting");
+ // infnite loop that if the connection is not 
+  while (true){ 
+    if(WiFi.status() == WL_CONNECTED){
+     Serial.println(" ");
+     Serial.println(" ");
+     Serial.println("WiFi connection Successful");
+  
+   break;   
+   }
+     delay(500);
+     Serial.print("*");
+  }
+ 
+  Serial.println("The IP Address of ESP8266 Module is: ");
+  Serial.println(WiFi.localIP());// Print the IP address 
+  Serial.println(NET_SSID);
+   }
+
+   void set_SSID(String ssid){
+ NET_SSID = ssid ;  
+}
+
+void set_password(String  password){
+ NET_PASSWORD = password; 
+}
+
+};
+
+
---
+++ sketch_feb25b.ino
@@ -0,0 +1,18 @@
+#include "myesp.h" 
+
+
+void setup() {
+WIFI esp ;
+String net_SSID = "END" ;
+String net_PASSWORD = "MasterEND" ;
+  esp.set_SSID(net_SSID);
+   esp.set_password(net_PASSWORD);
+  
+ Serial.begin(9600);
+esp.connection();
+
+}
+
+void loop() {
+ 
+}
